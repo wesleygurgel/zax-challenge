3ed57b8ed8dc161a3c20cbdeb9edfaa7d5f26d2f
Wesley Gurgel <wesleygurgel27@gmail.com>
2023-08-11 17:20:12 -0300

    feat: docstring and typehint for strategies

--- src/strategies/distribution_strategy.py
+++ src/strategies/distribution_strategy.py
@@ -1,36 +1,86 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Deque
+from collections import deque
+
+from src.models.motoboy import Motoboy
+from src.models.store import Store
+from src.models.order import Order
+
 from src.utils import rules as rules_methods
 
 
-class Context():
+class Context:
+    """
+    Define a interface de execução da estratégia de distribuição.
+    """
 
     def __init__(self, strategy: DistributionStrategy = None) -> None:
+        """
+        Inicializa o contexto com uma estratégia de distribuição opcional.
+
+        :param strategy: Estratégia de distribuição (opcional)
+        """
         self._strategy = strategy
 
     @property
     def strategy(self) -> DistributionStrategy:
+        """
+        :return: Retorna a estratégia de distribuição atual
+        """
         return self._strategy
 
     @strategy.setter
     def strategy(self, strategy: DistributionStrategy) -> None:
+        """
+        Define uma nova estratégia de distribuição.
+
+        :param strategy: Nova estratégia de distribuição
+        """
         self._strategy = strategy
 
-    def exec_distribution(self, motoboys, stores) -> None:
+    def exec_distribution(self, motoboys: List[Motoboy], stores: List[Store]) -> None:
+        """
+        Executa a distribuição de pedidos usando a estratégia definida.
+
+        :param motoboys: Lista de motoboys
+        :param stores: Lista de lojas
+        """
         self._strategy.distribute(motoboys, stores)
 
 
 class DistributionStrategy(ABC):
+    """
+    Interface base para estratégias de distribuição.
+    """
 
     @abstractmethod
-    def distribute(self, motoboys, stores):
+    def distribute(self, motoboys: List[Motoboy], stores: List[Store]) -> None:
+        """
+        Método abstrato para distribuir pedidos.
+
+        :param motoboys: Lista de motoboys
+        :param stores: Lista de lojas
+        """
         pass
 
-    def distribute_remaining_orders_equally(self, motoboys, orders_queue, rule: str) -> List[Motoboy]:
+    def distribute_remaining_orders_equally(self, motoboys: List[Motoboy], orders_queue: Deque[Order], rule: str) -> List[Motoboy]:
+        """
+        Distribui os pedidos restantes igualmente entre os motoboys.
+
+        Por se tratar de uma função auxiliar, se faz necessário passar a regra de distribuição como parâmetro: "rule".
+
+        Assim podemos remover o Motoboy que foi contemplado com a Estratégia de Distribuição prioritária.
+
+        :param motoboys: Lista de motoboys
+        :param orders_queue: Fila de pedidos restantes (utilizando collections.deque)
+        :param rule: Nome do método dentro do módulo src/utils/rules.py
+        :return: Lista de motoboys com pedidos distribuídos
+        """
         motoboy_index = 0
         while orders_queue:
             motoboy = motoboys[motoboy_index]
             if not getattr(rules_methods, rule)(motoboy):
                 motoboy.add_order(orders_queue.popleft())
             motoboy_index = (motoboy_index + 1) % len(motoboys)
+        return motoboys
--- src/strategies/exclusive_first_strategy.py
+++ src/strategies/exclusive_first_strategy.py
@@ -1,11 +1,25 @@
 from collections import deque
-from src.strategies.distribution_strategy import DistributionStrategy
+from typing import List, Deque
+
 from src.constants import EXCLUSIVE_STRATEGY
+from src.strategies.distribution_strategy import DistributionStrategy
+
+from src.models.motoboy import Motoboy
+from src.models.store import Store
+from src.models.order import Order
 
 
 class ExclusiveFirstStrategy(DistributionStrategy):
-    def distribute(self, motoboys, stores):
-        orders_queue = deque([order for store in stores for order in store.orders])
+    def distribute(self, motoboys: List[Motoboy], stores: List[Store]) -> List[Motoboy]:
+        """
+        Distribui os pedidos, dando prioridade aos pedidos exclusivos.
+
+        :param motoboys: Lista de motoboys
+        :param stores: Lista de lojas
+        :return: Lista de motoboys com pedidos distribuídos
+        """
+        orders_queue = deque(
+            [order for store in stores for order in store.orders])
 
         # Distribute exclusive orders
         self.distribute_exclusive_orders(motoboys, stores, orders_queue)
@@ -15,7 +29,14 @@ class ExclusiveFirstStrategy(DistributionStrategy):
 
         return motoboys
 
-    def distribute_exclusive_orders(self, motoboys, stores, orders_queue):
+    def distribute_exclusive_orders(self, motoboys: List[Motoboy], stores: List[Store], orders_queue: Deque[Order]) -> None:
+        """
+        Distribui os pedidos exclusivos entre os motoboys e lojas.
+
+        :param motoboys: Lista de motoboys
+        :param stores: Lista de lojas
+        :param orders_queue: Fila de pedidos restantes (utilizando collections.deque)
+        """
         for motoboy in motoboys:
             if motoboy.exclusive_store:
                 for order in [order for store in stores if store.id == motoboy.exclusive_store for order in store.orders]:
